da6d64210ff4225172315c0eefb26aa9470a5af0
Azat201003 <zanelane87@gmail.com>
2024-05-30 19:54:30 +0400

    change bullet2

--- scripts/bullet2.gd
+++ scripts/bullet2.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 var speed = 0
 const attack = 1./20.
 var timer = Timer.new()
+@onready
+var spawnPos = position
 
 func _process(_delta):
-	if sqrt(position.x ** 2 + position.y ** 2) > 10000:
-		queue_free()
+	if sqrt((spawnPos.x - position.x) ** 2 + (spawnPos.y - position.y) ** 2) > 700:
+		collide()
 	move_and_slide()
 
 func destroy():
